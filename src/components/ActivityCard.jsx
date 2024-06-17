0a2bb670224fb15385b5dafde24b755d12e8f23c
FevenSeyfu <fevenseyfu00@gmail.com>
2024-06-17 11:10:39 +0300

    Fix accessability warnings

--- src/components/ActivityCard.jsx
+++ src/components/ActivityCard.jsx
@@ -10,20 +10,20 @@ const ActivityCard = ({ activity }) => {
   return (
     <article className={`activity-card ${activity.title.toLowerCase()}`}>
       <div className="activity-content">
-        <section className="heading">
+        <div className="heading">
           <h2>{activity.title}</h2>
-          <button>
+          <button aria-label="More options">
             <IconEllipsis className="icon-ellipsis"/> 
           </button>
-        </section>
-        <section className="activity-record">
+        </div>
+        <div className="activity-record">
           <p className="current">
             {activity.timeframes[activityDuration].current}hrs
           </p>
           <p className="previous">
             Previous - {activity.timeframes[activityDuration].previous}hrs
           </p>
-        </section>
+        </div>
       </div>
     </article>
   );
